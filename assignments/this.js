d730b311b751d5640496d314ca2cbdad04710c1e
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-01-23 13:58:29 -0700

    'In my own words' Finished

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. In Window Binding, "this" refers to the actual browser window.
+* 2. For Implicit Binding, "this" refers to whatever is left of the dot when the object is being called or invoked.
+* 3. In New Binding, "this" refers to the new object being made by invoking the constructor object and passing it the key/value pairs as arguments.
+* 4. For Explicit Bindind, "this" will only refer to both the object and function (if the function is taken out of the object) when you invoke the function with a method such as .call().apply().bind and passing the object and function in as parameters.
 *
 * write out a code example of each explanation above
 */
